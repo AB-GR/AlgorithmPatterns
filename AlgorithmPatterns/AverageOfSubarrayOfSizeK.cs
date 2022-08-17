fb1ccff6dd07938ef368776f7b7ab18ef0da2825
Abhilash <abhilashgraja@gmail.com>
2022-08-17 15:11:26 +0530

    AverageOfSubarrayOfSizeK

---
+++ AlgorithmPatterns/AverageOfSubarrayOfSizeK.cs
@@ -0,0 +1,25 @@
+﻿namespace AlgorithmPatterns
+{
+	public class AverageOfSubarrayOfSizeK
+	{
+		public double[] Run(int[] arr, int k)
+		{
+			var result = new double[arr.Length - k + 1];
+			double sum = 0;
+			var i = 0;
+			for (int j = 0; j < arr.Length; j++)
+			{
+				sum += arr[j];
+
+				if(j >= k - 1)
+				{
+					result[i] = sum / k;
+					sum -= arr[i];
+					i++;
+				}
+			}
+
+			return result;
+		}
+	}
+}
--- AlgorithmPatterns/Program.cs
+++ AlgorithmPatterns/Program.cs
@@ -4,8 +4,10 @@
 	{
 		static void Main(string[] args)
 		{
-			var sm = new SmallestSubArrayWithGreaterSum();
-			sm.Run();
+			var av = new AverageOfSubarrayOfSizeK();
+			var result = av.Run(new int[] { 1, 3, 2, 6, -1, 4, 1, 8, 2 }, 5);
+			//var sm = new SmallestSubArrayWithGreaterSum();
+			//sm.Run();
 			Console.ReadLine();
 		}
 	}
